34dead315b9720e47b2799af017f1287fcd3822c
cohenshira <shiraxcohen@gmail.com>
2022-03-03 13:54:39 +0200

    Fixings

--- modules/vpn-gateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "object" {
-  value = azurerm_virtual_network_gateway.gateway
-}
-output "name" {
-  value = azurerm_virtual_network_gateway.gateway.name
-}
-output "id" {
-  value = azurerm_virtual_network_gateway.gateway.id
-}
