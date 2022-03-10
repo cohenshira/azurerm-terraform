9ef6814a02b3721d407612c1283cf42b9f4f91ad
cohenshira <shiraxcohen@gmail.com>
2022-03-10 16:17:47 +0200

    Final Changes

--- modules/firewall/examples/example.tf
+++ modules/firewall/examples/example.tf
@@ -1,13 +1,38 @@
 module "firewall" {
   source = "./modules/firewall"
 
-  location                            = "westeurope"
-  resource_group_name                 = "example-resource-group"
-  firewall_name                       = "example-firewall-name"
-  firewall_policy_name                = "example-firewall-policy"
-  subnet_id                           = "/subscriptions/xxx/resourceGroups/example-resource-group/providers/Microsoft.Network/virtualNetworks/example-virtual-network/subnets/AzureFirewallSubnet"
-  app_rule_collection_groups          = {}
-  network_rule_collection_groups      = {
+  location                       = "westeurope"
+  resource_group_name            = "example-resource-group"
+  firewall_name                  = "example-firewall-name"
+  firewall_policy_name           = "example-firewall-policy"
+  subnet_id                      = "/subscriptions/xxx/resourceGroups/example-resource-group/providers/Microsoft.Network/virtualNetworks/example-virtual-network/subnets/AzureFirewallSubnet"
+  app_rule_collection_groups     = {
+    name     = "application_rule_collection_group"
+    priority = 102
+
+    application_rule_collections = {
+      example-application-rule-collection = {
+        name     = "example-application-rule-collection"
+        priority = 103
+        action   = "Allow"
+
+        rules = {
+          allow-http = {
+            name             = "AllowHttp"
+            source_addresses = "10.0.0.0/16"
+            target_fqdns     = "*.micrrosoft.com"
+            protocols        = {
+              http = {
+                protocol_type = "Http"
+                port          = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  network_rule_collection_groups = {
     name     = "network_rule_collection_group"
     priority = 105
 
@@ -26,5 +51,28 @@ module "firewall" {
       }
     }
   }
-  nat_rule_collection_groups          = {}
+  nat_rule_collection_groups     = {
+    name     = "nat-rule-collection-group"
+    priority = 300
+
+    nat_rule_collections = {
+      example-nat-nat_rule_collection = {
+        name     = "example-nat-rule-collection"
+        priority = 301
+        action   = "Dnat"
+
+        rules = {
+          nat_rule = {
+            name               = "nat-rule"
+            protocols          = ["TCP"]
+            source_addresses   = ["*"]
+            destination_ports  = ["80"]
+            translated_port    = "8080"
+            translated_address = "10.0.0.5"
+          }
+        }
+
+      }
+    }
+  }
 }
--- modules/firewall/modules/firewall-policy/examples/example.tf
+++ modules/firewall/modules/firewall-policy/examples/example.tf
@@ -1,10 +1,62 @@
 module "firewall_policy" {
   source = "./modules/firewall-policy"
 
-  location                           = "westeurope"
-  resource_group_name                = "example-resource-group"
-  firewall_policy_name               = "example-firewall-policy"
-  network_rule_collection_groups     = {
+  location                   = "westeurope"
+  resource_group_name        = "example-resource-group"
+  firewall_policy_name       = "example-firewall-policy"
+  app_rule_collection_groups = {
+    name     = "application_rule_collection_group"
+    priority = 102
+
+    application_rule_collections = {
+      example-application-rule-collection = {
+        name     = "example-application-rule-collection"
+        priority = 103
+        action   = "Allow"
+
+        rules = {
+          allow-http = {
+            name             = "AllowHttp"
+            source_addresses = "10.0.0.0/16"
+            target_fqdns     = "*.micrrosoft.com"
+            protocols        = {
+              http = {
+                protocol_type = "Http"
+                port          = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  nat_rule_collection_groups = {
+    name     = "nat-rule-collection-group"
+    priority = 300
+
+    nat_rule_collections = {
+      example-nat-nat_rule_collection = {
+        name     = "example-nat-rule-collection"
+        priority = 301
+        action   = "Dnat"
+
+        rules = {
+          nat_rule = {
+            name               = "nat-rule"
+            protocols          = ["TCP"]
+            source_addresses   = ["*"]
+            destination_ports  = ["80"]
+            translated_port    = "8080"
+            translated_address = "10.0.0.5"
+          }
+        }
+
+      }
+    }
+  }
+
+  network_rule_collection_groups = {
     name     = "network_rule_collection_group"
     priority = 105
 
@@ -23,6 +75,6 @@ module "firewall_policy" {
       }
     }
   }
-  
-  firewall_public_ip                 = azurerm_public_ip.pip.ip_address
+
+  firewall_public_ip = azurerm_public_ip.pip.ip_address
 }
